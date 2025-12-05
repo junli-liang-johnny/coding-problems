3bea2ea9191d8d4ca567802b55f46364d1e446cb
Junli Liang <junli.liang@ucd.ie>
2025-12-05 17:47:25 +0000

    added question

---
+++ medium/567_permutation_in_string.py
@@ -0,0 +1,26 @@
+"""
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+
+"""
+
+class Sol:
+	def sol(self, s1: str, s2: str) -> bool:
+		pass
+
+if __name__ == "__main__":
+	sol = Sol()
+	print(sol.sol("ab", "eidbaooo"))
